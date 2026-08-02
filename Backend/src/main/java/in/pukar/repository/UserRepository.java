207631619797a3d01332fc89f33c3d46210bb492
I-am-ankita <ankita.yadav.cmfeb26@email.com>
2026-08-02 22:47:27 +0530

    Add AuthController,UserRepository and AppUserDetailsService

---
+++ Backend/src/main/java/in/pukar/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package in.pukar.repository;
+
+import in.pukar.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    List<User> findByDepartmentId(String departmentId);
+    List<User> findByWardIdAndDepartmentId(String wardId, String departmentId);
+    List<User> findByWardId(String wardId);
+}
